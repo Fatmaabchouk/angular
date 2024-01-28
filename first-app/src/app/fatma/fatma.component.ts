3e1416dffd3e6364fcbc92441e524d500a1a23d2
Fatmaabchouk <abchoukfatma@gmail.com>
2024-01-28 19:59:16 +0100

    first commit

---
+++ first-app/src/app/fatma/fatma.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fatma',
+  standalone: true,
+  imports: [],
+  templateUrl: './fatma.component.html',
+  styleUrl: './fatma.component.css'
+})
+export class FatmaComponent implements OnInit{
+constructor(){}
+ngOnInit(): void {
+  
+}
+}
